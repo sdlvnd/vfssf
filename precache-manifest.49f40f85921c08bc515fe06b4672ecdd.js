5435c545e860b4c963a656123caeecc85a874683
ertovkax <s280906k@gmail.com>
2021-11-26 23:05:40 +0300

    Updates

---
+++ precache-manifest.49f40f85921c08bc515fe06b4672ecdd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a879a828436b83eb3172a0d475210e2",
+    "url": "/djkdjhc/index.html"
+  },
+  {
+    "revision": "281379ab4d6dc2dbf811",
+    "url": "/djkdjhc/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "9672509f265c5bde48f9",
+    "url": "/djkdjhc/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "281379ab4d6dc2dbf811",
+    "url": "/djkdjhc/static/js/2.2bd40b5a.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/djkdjhc/static/js/2.2bd40b5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9672509f265c5bde48f9",
+    "url": "/djkdjhc/static/js/main.c9fee35b.chunk.js"
+  },
+  {
+    "revision": "ca60dbc2d3a502b14dc8",
+    "url": "/djkdjhc/static/js/runtime-main.e96f616c.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/djkdjhc/static/media/persik.4e1ec840.png"
+  }
+]);
